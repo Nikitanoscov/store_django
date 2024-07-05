0fb6118aeb1733d8be52960478eb9eaca623c446
Nikita Noskov <nik030205@mail.ru>
2024-07-05 14:29:54 +0300

    Debug-toolbar connection.

--- store_app/store_app/settings.py
+++ store_app/store_app/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS: list[str] = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -38,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+
     'goods',
     'main',
 ]
@@ -50,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'store_app.urls'
--- store_app/store_app/urls.py
+++ store_app/store_app/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='goods'))
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
